dc3b85e6103c27c1b316c037d575ba8d97a1f3e4
frank89722 <frank89722@gmail.com>
2017-09-08 11:55:58 +0800

    upload

---
+++ Assets/_Completed-Game/Scripts/CubeControll.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeControll : MonoBehaviour {
+	public float mSpeed;
+	// Use this for initialization
+	void Start () {
+		mSpeed = 1f;
+	}
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (mSpeed * Input.GetAxis ("Horizontal") * Time.deltaTime, 0f, mSpeed * Input.GetAxis ("Vertical") * Time.deltaTime);
+	}
+}
