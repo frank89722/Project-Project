b6ec276f3e92a772d239abfa5d195f37aa79f9c4
frank89722 <frank89722@gmail.com>
2017-09-15 10:50:15 +0800

    Object move and rotate

--- Assets/_Completed-Game/Scripts/CubeControll.cs
+++ Assets/_Completed-Game/Scripts/CubeControll.cs
@@ -6,11 +6,30 @@ public class CubeControll : MonoBehaviour {
 	public float mSpeed;
 	// Use this for initialization
 	void Start () {
-		mSpeed = 1f;
+		mSpeed = 0.3f;
 	}
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (mSpeed * Input.GetAxis ("Horizontal") * Time.deltaTime, 0f, mSpeed * Input.GetAxis ("Vertical") * Time.deltaTime);
+		//transform.Translate (mSpeed * Input.GetAxis ("Horizontal") * Time.deltaTime, 0f, mSpeed * Input.GetAxis ("Vertical") * Time.deltaTime);
+
+		if (Input.GetKey (KeyCode.W) & Input.GetKey (KeyCode.D)) {
+			gameObject.transform.rotation = Quaternion.Euler (0f, 45f, 0f);
+			transform.position += Vector3.forward * mSpeed;
+		} else if (Input.GetKey (KeyCode.S) & Input.GetKey (KeyCode.D)) {
+			gameObject.transform.rotation = Quaternion.Euler (0f, 135f, 0f);
+		} else if (Input.GetKey (KeyCode.A) & Input.GetKey (KeyCode.S)) {
+			gameObject.transform.rotation = Quaternion.Euler (0f, 225f, 0f);
+		} else if (Input.GetKey (KeyCode.A) & Input.GetKey (KeyCode.W)) {
+			gameObject.transform.rotation = Quaternion.Euler (0f, 315f, 0f);
+		} else if (Input.GetKey (KeyCode.W)) {
+			gameObject.transform.rotation = Quaternion.Euler (0f, 0f, 0f);
+		} else if (Input.GetKey (KeyCode.A)) {
+			gameObject.transform.rotation = Quaternion.Euler (0f, 270f, 0f);
+		} else if (Input.GetKey (KeyCode.S)) {
+			gameObject.transform.rotation = Quaternion.Euler (0f, 180f, 0f);
+		} else if (Input.GetKey (KeyCode.D)) {
+			transform.position += Vector3.forward * mSpeed;
+			gameObject.transform.rotation = Quaternion.Euler (0f, 90f, 0f);
+		}
 	}
-	//123
 }
---
+++ Assets/_Completed-Game/Scripts/TestCube.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+public class TestCube : MonoBehaviour {
+	public GameObject model;
+	private Animator animator;
+	public GameObject [] target = new GameObject[8];
+	public float speed;
+	void Start () {
+		animator = model.GetComponent<Animator> ();
+	}
+
+	void Update () {
+		animator.SetBool("RunState",false);////(1)
+		if(Input.GetKey(KeyCode.W)){////(2)
+			Vector3 targetDir = target[0].transform.position - transform.position;
+			float step = speed * Time.deltaTime;
+			Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+			transform.rotation = Quaternion.LookRotation(newDir);
+			animator.SetBool("RunState",true);////(1)
+		}
+		else if(Input.GetKey(KeyCode.S)){////(2)
+
+			Vector3 targetDir = target[4].transform.position - transform.position;
+			float step = speed * Time.deltaTime;
+			Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+			transform.rotation = Quaternion.LookRotation(newDir);
+			animator.SetBool("RunState",true);
+		}
+		else if(Input.GetKey(KeyCode.A)){////(2)
+
+			Vector3 targetDir = target[6].transform.position - transform.position;
+			float step = speed * Time.deltaTime;
+			Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+			transform.rotation = Quaternion.LookRotation(newDir);
+			animator.SetBool("RunState",true);
+		}
+		else if(Input.GetKey(KeyCode.D)){////(2)
+
+			Vector3 targetDir = target[2].transform.position - transform.position;
+			float step = speed * Time.deltaTime;
+			Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+			transform.rotation = Quaternion.LookRotation(newDir);
+			animator.SetBool("RunState",true);
+		}
+	}
+}
