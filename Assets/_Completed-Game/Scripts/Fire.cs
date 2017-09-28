d69a9568a708adedbd706c7fab389b56484cca33
GGININDER5487 <31754503+GGININDER5487@users.noreply.github.com>
2017-09-29 06:05:04 +0800

    GGININDER

--- Assets/_Completed-Game/Scripts/Fire.cs
+++ Assets/_Completed-Game/Scripts/Fire.cs
@@ -2,33 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fire : MonoBehaviour {
-	public GameObject bulletPrefab;
-	public Transform bulletSpawn;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.Space)){
-    		Kire();
-		}
-	}
-	void Kire(){
-    // Create the Bullet from the Bullet Prefab
-    var bullet = (GameObject)Instantiate (
-        bulletPrefab,
-        bulletSpawn.position,
-        bulletSpawn.rotation);
+public class Fire : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+ 
+    public float speed = 1;
+    // Use this for initialization
+    void Start()
+    {
 
-    // Add velocity to the bullet
-    bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+    }
 
-    // Destroy the bullet after 2 seconds
-    Destroy(bullet, 2.0f);
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            kire();
+        }
+
+    }
+    void kire()
+    {
+        // Create the Bullet from the Bullet Prefab
+
+        var bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.transform.Translate(0, 0, 70 * Time.deltaTime);
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
 }
 
 
-}
+
