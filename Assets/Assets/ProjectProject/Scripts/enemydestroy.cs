f6235ab6840ce56f9cdfdafd219e79e680ac8cd0
frank89722 <frank89722@gmail.com>
2017-10-27 11:46:05 +0800

    sagiri

---
+++ Assets/Assets/ProjectProject/Scripts/enemydestroy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class enemydestroy : MonoBehaviour {
+	public int heal;
+	// Use this for initialization
+	void Start () {
+		heal=20;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+    }
+    void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Pick Up")) heal--;
+		if (heal == 0) Destroy (gameObject);
+		else if (heal <= 8) GetComponent<Renderer>().material.color = Color.red;
+		else if (heal <= 14) GetComponent<Renderer>().material.color = Color.yellow;
+ 	}
+}
---
+++ Assets/Assets/UIScript/LevelManger.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LevelManger : MonoBehaviour {
+
+	public void LoadGame(string name){
+		Application.LoadLevel (name);
+	}
+}
