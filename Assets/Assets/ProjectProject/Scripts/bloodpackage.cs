a6139427bcc4d617a8c65c7a61a80a7fb5db6af5
GGININDER5487 <31754503+GGININDER5487@users.noreply.github.com>
2017-11-24 22:23:54 +0800

    bloodbackage

---
+++ Assets/Assets/ProjectProject/Scripts/bloodpackage.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class bloodpackage : MonoBehaviour
+    {
+        public GameObject Game;
+        public GameObject package;
+        public int s;
+        public float b;
+        public float x;
+
+        // Use this for initialization
+        void Start()
+        {
+            s = 45;
+            x = 0;
+            b = 0.4f;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (s <= 360)
+            {
+                package.transform.rotation = Quaternion.Euler(0, s, 45);
+                package.transform.position = new Vector3(0, b * Time.deltaTime, 0);
+                s += 1;
+
+            }
+            else if (s >= 360)
+            {
+                s = 0;
+                package.transform.rotation = Quaternion.Euler(0, s, 45);
+                package.transform.position = new Vector3(0, -b * Time.deltaTime, 0);
+        }
+            if (x >= 150)
+            {
+                Instantiate(Game, new Vector3(Random.Range(-8f, 8f), 0.5f, Random.Range(-8f, 8f)), Game.transform.rotation);
+                x = 0;
+            }
+            else
+            {
+                x += 1;
+            }
+        }
+    }
