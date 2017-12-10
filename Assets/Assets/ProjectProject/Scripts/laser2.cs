72f4f0c0c6ab4a2898c705ed260040f32745679e
GGININDER5487 <31754503+GGININDER5487@users.noreply.github.com>
2017-12-11 01:33:23 +0800

    local laser

---
+++ Assets/Assets/ProjectProject/Scripts/laser2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laser2 : MonoBehaviour
+{
+    public GameObject Game;
+    public GameObject laser;
+    public int s;
+    public float b;
+    public int g;
+    public int timerx;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+       g = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        g++;
+        laser.transform.Rotate(0, 3, 0);
+   /*     if (g >= 100)
+        {
+            Destroy(GameObject.FindWithTag("wow"));
+        }*/
+        if (g >= 300)
+        {
+            var hpobj = Instantiate(Game, new Vector3(Random.Range(-6f, 6f), 0.5f, Random.Range(-6f, 6f)), Game.transform.rotation);
+            hpobj.gameObject.tag = "wow";
+            g = 0;
+        }
+    }
+}
+
